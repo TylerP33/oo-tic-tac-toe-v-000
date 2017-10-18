1b9a641ef73cc9dafe7910617823133d660baf7a
Tyler Pulse <tyler3321@gmail.com>
2017-10-18 16:44:09 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -44,8 +44,8 @@ def display_board
 
 
 if valid_move?(index)
-    current_player
-    move(index, current_player = "X")
+    current_player == "X" || "O"
+    move(index, current_player)
     display_board
   else
     turn
