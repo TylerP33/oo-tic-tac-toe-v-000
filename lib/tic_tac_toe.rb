0585e8a36b8d6ca719c0803818f7ee640c5d6f6b
Tyler Pulse <tyler3321@gmail.com>
2017-10-18 16:46:19 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -40,7 +40,7 @@ def display_board
   puts "Please enter 1-9:"
   user_input = gets.chomp
   index = input_to_index(user_input)
-  current_player = current_player(index)
+  current_player = current_player
 
 
 
